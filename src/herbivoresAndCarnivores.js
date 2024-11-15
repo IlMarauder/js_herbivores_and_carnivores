3145ba9494d97f94883a083b6401d17da0426171
Ilya Morderer <ilya.morderer@gmail.com>
2024-11-15 10:02:31 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,40 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(bitten) {
+    if (bitten.hidden === false) {
+      bitten.health -= 50;
+    }
+
+    if (bitten.health <= 0) {
+      const indexOfDead = Animal.alive.indexOf(bitten);
+
+      Animal.alive.splice(indexOfDead, 1);
+    }
+  }
 }
 
 module.exports = {
